badb2b9af2988764f82ecbf8e9c10b774621613b
anshika <anshika2311sharma@gmail.com>
2025-05-29 16:26:45 +0530

    start

---
+++ math.js
@@ -0,0 +1,9 @@
+const sum=(a,b)=>a+b;
+const PI=3.14
+
+let obj={
+    sum: sum,
+    PI:PI,
+    
+}
+module.exports=obj;
---
+++ script.js
@@ -0,0 +1,19 @@
+
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log("hello",i);
+
+// }
+// console.log(process.argv)
+
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+// console.log("hello",args[i]);
+// }
+
+// const val=require("./math");
+// console.log(val);
+
+const val=require("./math");
+console.log(val);
+console.log(Math.PI)
